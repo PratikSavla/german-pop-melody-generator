7ae62392744de04b14df92a06e31ed78425effae
PratikSavla <pratiksavla07@gmail.com>
2020-07-26 11:06:29 +0530

    add code

---
+++ Readme.md
@@ -0,0 +1,174 @@
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+[![MIT License][license-shield]][license-url]
+[![LinkedIn][linkedin-shield]][linkedin-url]
+
+
+
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+  <!--
+  <a href="https://github.com/othneildrew/Best-README-Template">
+    <img src="images/logo.png" alt="Logo" width="80" height="80">
+  </a>
+  -->
+  <h3 align="center">GermanMelGen</h3>
+
+  <p align="center">
+    An awesome AI model to generate German Pop melodies
+    <br />
+    <a href="https://github.com/PratikSavla/GermanMelGen"><strong>Explore the docs »</strong></a>
+    <br />
+    <br />
+    <a href="https://github.com/PratikSavla/GermanMelGen">View Demo</a>
+    ·
+    <a href="https://github.com/PratikSavla/GermanMelGen/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/PratikSavla/GermanMelGen/issues">Request Feature</a>
+  </p>
+</p>
+
+
+
+<!-- TABLE OF CONTENTS -->
+## Table of Contents
+
+* [About the Project](#about-the-project)
+  * [Built With](#built-with)
+* [Getting Started](#getting-started)
+  * [Prerequisites](#prerequisites)
+  * [Installation](#installation)
+* [Usage](#usage)
+* [Roadmap](#roadmap)
+* [Contributing](#contributing)
+* [License](#license)
+* [Contact](#contact)
+* [Acknowledgements](#acknowledgements)
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+<!-- [![Product Name Screen Shot][product-screenshot]](https://example.com) -->
+
+I have made many projects in AI. This time I decided to make something very simple and fun in this time. This is an LSTM model trained on [German Pop Melodies](https://kern.humdrum.org). It is an amazing way to learn the basics of LSTMs and music data preprocessing.
+
+A list of commonly used resources that I find helpful are listed in the acknowledgements.
+
+
+
+### Built With
+This section should list any major frameworks that you built your project using. Leave any add-ons/plugins for the acknowledgements section. Here are a few examples.
+* [Python](https://www.anaconda.com/)
+* [Tensorflow](https://www.tensorflow.org/)
+* [Music21](http://web.mit.edu/music21/)
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+
+* [Anaconda Python](https://www.anaconda.com/)
+* [Musescore](https://musescore.org/en)
+
+
+
+### Installation
+
+1. Clone the repo
+```sh
+git clone https://github.com/PratikSavla/GermanMelGen.git
+```
+2. Create Conda Virtual Environment
+```sh
+cd GermanMelGen
+conda create -n germelgen python=3.7 anaconda
+```
+3. Activate your virtual environment
+```JS
+source activate germelgen
+```
+4. Install packages
+```sh
+conda install --force-reinstall -y --name germelgen --file requirements.txt
+```
+5. Additional requirements may be needed to setup musescore.
+
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+1. Download the [dataset](https://kern.humdrum.org/cgi-bin/ksdata?l=essen/europa/deutschl&format=recursive) for training the model
+```sh
+python preprocess.py
+python train.py
+```
+2. Use Pretrained [Model](link) to generate melody.
+```sh
+python melodygenerator.py
+```
+
+
+
+<!-- ROADMAP -->
+## Roadmap
+
+See the [open issues](https://github.com/PratikSavla/GermanMelGen/issues) for a list of proposed features (and known issues).
+
+
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+
+
+<!-- CONTACT -->
+## Contact
+
+Your Name - [@pratik_savla_](https://www.linkedin.com/in/pratik-savla-b5973815b/) - pratiksavla07@gmail.com
+
+Project Link: [https://github.com/PratikSavla/GermanMelGen](https://github.com/PratikSavla/GermanMelGen)
+
+
+
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[contributors-shield]: https://img.shields.io/github/contributors/othneildrew/Best-README-Template.svg?style=flat-square
+[contributors-url]: https://github.com/othneildrew/Best-README-Template/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/othneildrew/Best-README-Template.svg?style=flat-square
+[forks-url]: https://github.com/othneildrew/Best-README-Template/network/members
+[stars-shield]: https://img.shields.io/github/stars/othneildrew/Best-README-Template.svg?style=flat-square
+[stars-url]: https://github.com/othneildrew/Best-README-Template/stargazers
+[issues-shield]: https://img.shields.io/github/issues/othneildrew/Best-README-Template.svg?style=flat-square
+[issues-url]: https://github.com/othneildrew/Best-README-Template/issues
+[license-shield]: https://img.shields.io/github/license/othneildrew/Best-README-Template.svg?style=flat-square
+[license-url]: https://github.com/othneildrew/Best-README-Template/blob/master/LICENSE.txt
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=flat-square&logo=linkedin&colorB=555
+[linkedin-url]: https://linkedin.com/in/othneildrew
+[product-screenshot]: images/screenshot.png
---
+++ preprocess.py
@@ -0,0 +1,201 @@
+import os
+import json
+import numpy as np
+import tensorflow.keras as keras
+import music21 as m21
+
+# us = m21.environment.UserSettings()
+# # # us.create()
+# us['musicxmlSettings'] = 'C:/Program Files/MuseScore 3/bin/MuseScore.exe'
+# us['musicxmlPath'] = 'C:/Program Files/MuseScore 3/bin/MuseScore3.exe'
+# us['musescoreDirectPNGPath'] = 'C:/Program Files/MuseScore 3/bin'
+# # us['midiPath'] = 'D:/Jul 2020/MelodGen'
+
+
+KERN_DATASET_PATH = "deutschl/erk"
+SAVE_DIR = "dataset"
+SINGLE_FILE_DATASET = "file_dataset"
+MAPPING_PATH = "mapping.json"
+SEQUENCE_LENGTH = 64
+
+ACCEPTABLE_DURATIONS = [
+    0.25, 0.5, 0.75, 1.0, 1.5, 2, 3, 4
+]
+
+def load_songs_in_kern(dataset_path):
+
+    songs = []
+
+    # go through all the files in dataset and load them with music21
+    for path, subdir, files in os.walk(dataset_path):
+        for file in files:
+            if file[-3:] == "krn":
+                song = m21.converter.parse(os.path.join(path, file))
+                songs.append(song)
+
+    return songs
+
+def has_acceptable_durations(song, acceptable_durations):
+    for note in song.flat.notesAndRests:
+        if note.duration.quarterLength not in acceptable_durations:
+            return False
+    return True
+
+def transpose(song):
+
+    # get key from the song
+    parts = song.getElementsByClass(m21.stream.Part)
+    measures_part0 = parts[0].getElementsByClass(m21.stream.Measure)
+    key = measures_part0[0][4]
+
+    # estimate key using music21
+    if not isinstance(key, m21.key.Key):
+        key = song.analyze("key")
+
+    # get interval for transposition. E.g., Bmaj -> Cmaj
+    if key.mode == "major":
+        interval = m21.interval.Interval(key.tonic, m21.pitch.Pitch("C"))
+    elif key.mode == "minor":
+        interval = m21.interval.Interval(key.tonic, m21.pitch.Pitch("A"))
+
+    # transpose song by calculated interval
+    transposed_song = song.transpose(interval)
+
+    return transposed_song
+
+def encode_song(song, time_step=0.25):
+    
+    encoded_song = []
+
+    for event in song.flat.notesAndRests:
+
+        # handle notes
+        if isinstance(event, m21.note.Note):
+            symbol = event.pitch.midi
+        elif isinstance(event, m21.note.Rest):
+            symbol = "r"
+
+        # convert note/rests into time series notation
+        steps = int(event.duration.quarterLength / time_step)
+        for step in range(steps):
+            if step == 0:
+                encoded_song.append(symbol)
+            else:
+                encoded_song.append("_")
+    
+    # cast encoded song to a string
+    encoded_song = " ".join(map(str, encoded_song))
+
+    return encoded_song
+
+def preprocess(dataset_path):
+
+    # load the folk songs
+    print("Loading Songs....")
+    songs = load_songs_in_kern(dataset_path)
+    print(f'Loaded {len(songs)} songs.')
+    
+    for i, song in enumerate(songs): 
+
+        # filter out songs that have non-acceptable durations
+        if not has_acceptable_durations(song, ACCEPTABLE_DURATIONS):
+            continue
+
+        # transpose songs to Cmajor/Aminor
+        # song = transpose(song)
+    
+        # encode songs with music time series representation
+        encoded_song = encode_song(song)
+
+        # save songs to text file
+        save_path = os.path.join(SAVE_DIR, str(i))
+        with open(save_path, 'w') as fp:
+            fp.write(encoded_song)
+
+def load(file_path):
+    with open(file_path, 'r') as fp:
+        song = fp.read()
+    return song
+
+def create_single_file_dataset(dataset_path, file_dataset_path, sequence_length):
+    new_song_delimiter = "/ "*sequence_length
+    songs = "" 
+    
+    # load encoded songs and add delimiters
+    for path, _, files in os.walk(dataset_path):
+        for file in files:
+            file_path = os.path.join(path, file)
+            song = load(file_path)
+            songs = songs + song + " " + new_song_delimiter
+
+    songs = songs[:-1]
+
+    # save string that contains all dataset
+    with open(file_dataset_path, 'w') as fp:
+        fp.write(songs)
+
+    return songs
+
+def create_mapping(songs, mapping_path):
+    mappings = dict()
+
+    # identify the vocabulary
+    songs = songs.split()
+    vocabulary = list(set(songs))
+
+    # create mappings
+    for i, symbol in enumerate(vocabulary):
+        mappings[symbol] = i
+
+    # save vocab in a jason file
+    with open(mapping_path, 'w') as fp:
+        json.dump(mappings, fp, indent=4)
+
+def convert_songs_to_int(songs):
+
+    int_songs = []
+    # load mappings
+    with open(MAPPING_PATH, "r") as fp:
+        mappings = json.load(fp)
+
+    # cast songs string to a list
+    songs = songs.split()
+
+    # map songs to int
+    for symbol in songs:
+        int_songs.append(mappings[symbol])
+
+    return int_songs
+
+def generate_training_sequences(sequence_length):
+    # [5, 6, 7, 8, ....] -> i: [5, 6], t: 7; i: [6, 7], t: 8; ...
+
+    # load songs and map them to int
+    songs = load(SINGLE_FILE_DATASET)
+    int_songs = convert_songs_to_int(songs)
+
+    # generate the training sequences
+    inputs = []
+    targets = []
+    num_sequences = len(int_songs) - sequence_length
+    for i in range(num_sequences):
+        inputs.append(int_songs[i:i+sequence_length])
+        targets.append(int_songs[i+sequence_length])
+
+    # one-hot encode the sequences
+    vocabulary_size = len(set(int_songs))
+    inputs = keras.utils.to_categorical(inputs, num_classes=vocabulary_size)
+    targets = np.array(targets)
+
+    return inputs, targets
+
+
+def main():
+    preprocess(KERN_DATASET_PATH)
+    songs = create_single_file_dataset(SAVE_DIR, SINGLE_FILE_DATASET, SEQUENCE_LENGTH)
+    create_mapping(songs, MAPPING_PATH)
+    # inputs, targets = generate_training_sequences(SEQUENCE_LENGTH)
+    # a = 1
+    # print(inputs.shape)
+if __name__ == "__main__":
+    main()
